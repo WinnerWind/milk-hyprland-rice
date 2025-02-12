aa5f22c6ce2ce6f59cbfdc9c71bd2dd79c2070ac
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-02-12 21:05:19 +0530

    Add files via upload

---
+++ music_scroll.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# playerctl metadata --format "{{artist}} - {{title}}" | zscroll -l 30
+
+
+# see man zscroll for documentation of the following parameters
+zscroll -l 60 \
+        --delay 0.1 \
+        --scroll-padding "  " \
+        --match-command "`dirname $0`/get_spotify_status.sh --status" \
+        --match-text "Playing" "--scroll 1 --before-text=⏵" \
+        --match-text "Paused" "--scroll 0" \
+        --update-check true "`dirname $0`/get_spotify_status.sh" &
+
+wait
+
